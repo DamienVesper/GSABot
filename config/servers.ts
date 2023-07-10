586d496cc350de72b51731df36143120c58a4b52
Damien Vesper <ldamienvesper@gmail.com>
2023-07-10 03:21:09 -0500

    update servers to have log channel

--- config/servers.ts
+++ config/servers.ts
@@ -1,8 +1,13 @@
-const servers: Record<string, string> = {
-    [`CO-UST`]: `c87d300c-e132-4dcc-a04c-1966584e59c6`,
-    [`CO-US01`]: `d3bef952-a64c-45c4-8ec0-be4b6daf59fe`,
-    [`CO-US02`]: `beb15055-e853-4d1d-88a6-8fe027225184`,
-    [`CO-US03`]: `ae042024-a61b-41ea-903e-c997926b9ae2`
+interface Server {
+    uuid: string
+    logChannel?: string
+}
+
+const servers: Record<string, Server> = {
+    [`CO-UST`]: { uuid: `c87d300c-e132-4dcc-a04c-1966584e59c6` },
+    [`CO-US01`]: { uuid: `d3bef952-a64c-45c4-8ec0-be4b6daf59fe`, logChannel: `644370385025892374` },
+    [`CO-US02`]: { uuid: `beb15055-e853-4d1d-88a6-8fe027225184` },
+    [`CO-US03`]: { uuid: `ae042024-a61b-41ea-903e-c997926b9ae2` }
 };
 
 export default servers;
